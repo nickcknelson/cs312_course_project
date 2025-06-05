2b0b0cbc6dd894a9aba36f5970aff3693516d235
nickcknelson <nnelson6789@gmail.com>
2025-06-05 15:46:14 -0700

    Out of ideas

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,11 +13,51 @@ provider "aws" {
   region  = "us-west-2"
 }
 
+resource "aws_security_group" "minecraft" {
+  name        = "minecraft-sg"
+  description = "Allow Minecraft port"
+
+  ingress {
+    from_port   = 25565
+    to_port     = 25565
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-08d70e59c07c61a3a"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.minecraft.id]
+  associate_public_ip_address = true
+
+  key_name = aws_key_pair.course_key.key_name
 
   tags = {
     Name = "minecraft-server"
   }
+}
+resource "aws_key_pair" "course_key" {
+  key_name   = "mc-final"
+  public_key = file("/home/nnelson/.ssh/mc-final.pub")
+}
+
+
+output "public_ip" {
+  description = "Public IP of the Minecraft server"
+  value       = aws_instance.app_server.public_ip
 }
